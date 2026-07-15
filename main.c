f2db14e46e7e5c3af87d1313e7a1f91cff245100
Sabiha-Islam-Liza <254388702+sabiha-islam-liza@users.noreply.github.com>
2026-07-15 20:30:45 +0600

    Added  basic interface

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "complaint.h"
+ int main ()
+ {
+    int choice;
+
+    printf("------This is a Comlaint Management System------\n");
+    
+    printf("1.File a complaint.\n");
+    printf ("2. Track a complaint.\n");
+    printf ("3.Admin authentication.\n");
+    printf ("4.Exit\n");
+
+    return 0;
+ }
