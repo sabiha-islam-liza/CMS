b90bade80188e43681e280ac483bd2cc5d9a0a9d
Sabiha Islam Liza <254388702+sabiha-islam-liza@users.noreply.github.com>
2026-08-15 22:52:14 +0600

    Refactor MD5 context and add inline functions

--- MD5.h
+++ MD5.h
@@ -1,18 +1,17 @@
 #ifndef MD5_H
 #define MD5_H
 
-#include <stdio.h>
 #include <stdint.h>
-#include <string.h>
 
 typedef struct {
     uint32_t state[4];
     uint32_t count[2];
     unsigned char buffer[64];
-} MD5_CTX;
+} MD5_CTX_INLINE;
 
-void MD5_Init(MD5_CTX *context);
-void MD5_Update(MD5_CTX *context, const unsigned char *input, unsigned int inputLen);
-void MD5_Final(unsigned char digest[16], MD5_CTX *context);
+void MD5Init_Inline(MD5_CTX_INLINE *context);
+void MD5Update_Inline(MD5_CTX_INLINE *context, const unsigned char *input, unsigned int inputLen);
+void MD5Final_Inline(unsigned char digest[16], MD5_CTX_INLINE *context);
+void get_user_md5(const char *input, char *output_hash);
 
-#endif // MD5_H
+#endif
