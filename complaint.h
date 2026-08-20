6bef8a3d3a98c33f77305ffac1d14b8d2f7b1164
Sabiha Islam Liza <254388702+sabiha-islam-liza@users.noreply.github.com>
2026-08-20 22:14:57 +0600

    Update complaint.h

--- complaint.h
+++ complaint.h
@@ -51,15 +51,6 @@ void viewComplaintsByAssignedTeam(const char *team_name);
 void set_win_color(unsigned short color);
 void send_notification(const char *target_admin, int complaint_id, const char *msg);
 
-void notify_general_admin(int complaint_id, const char *category, const char *user_id);
-void notify_assigned_team(const char *teamName, int complaintID, const char *status);
-void notify_accused_person_updated(const char *targetUser, int complaintId, const char *category);
-void trigger_accused_popup_on_login(const char *logged_in_user);
-void notify_grievance_committee(int complaint_id, const char *user_id);
-void notify_filing_user(const char *user_id, int complaint_id, const char *category);
-
-void check_user_notifications(const char *username);
-void check_and_alert_accused(const char *username);
 
 
 
