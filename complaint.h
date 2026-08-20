ed5dbf933bc0259b8d38ec58c12feb003c558343
Sabiha Islam Liza <254388702+sabiha-islam-liza@users.noreply.github.com>
2026-08-20 22:13:03 +0600

    Update complaint.h

--- complaint.h
+++ complaint.h
@@ -1,10 +1,15 @@
 #ifndef COMPLAINT_H
 #define COMPLAINT_H
+#ifndef NOTIF_FILE
+#define NOTIF_FILE "notifications.txt"
+#endif
 #define FILE_NAME "complaints.csv"
+#define RECOVERY_FILE "user_recovery.txt"
 #define MAX 100
 #define ID_LEN 50
 #define PASS_LEN 50
 #define HASH_LEN 65
+
 //structure
 
 typedef struct {
@@ -44,4 +49,19 @@ void viewComplaints(const char *logged_in_user);  // user's portal view , they c
 void viewAccusedComplaints(const char *logged_in_user); // the accused person can see the complaint filed against him
 void viewComplaintsByAssignedTeam(const char *team_name);
 void set_win_color(unsigned short color);
+void send_notification(const char *target_admin, int complaint_id, const char *msg);
+
+void notify_general_admin(int complaint_id, const char *category, const char *user_id);
+void notify_assigned_team(const char *teamName, int complaintID, const char *status);
+void notify_accused_person_updated(const char *targetUser, int complaintId, const char *category);
+void trigger_accused_popup_on_login(const char *logged_in_user);
+void notify_grievance_committee(int complaint_id, const char *user_id);
+void notify_filing_user(const char *user_id, int complaint_id, const char *category);
+
+void check_user_notifications(const char *username);
+void check_and_alert_accused(const char *username);
+
+
+
+
 #endif
